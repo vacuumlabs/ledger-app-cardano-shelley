26b850977a29428a4ea0f55d5b03865b7f971814
Jakub Arbet <hi@jakubarbet.me>
2021-07-20 13:06:40 +0200

    Add UI for derive native script hash call

--- src/deriveNativeScriptHash.c
+++ src/deriveNativeScriptHash.c
@@ -1,5 +1,6 @@
 #include "deriveNativeScriptHash.h"
 #include "state.h"
+#include "uiScreens.h"
 
 enum nativeScriptType {
 	NATIVE_SCRIPT_PUBKEY = 0,
@@ -22,6 +23,14 @@ static ins_derive_native_script_hash_context_t* ctx = &(instructionState.deriveN
 
 // Helper functions
 
+bool expectsRequest()
+{
+	// if the number of remaining scripts is not bigger than 0, then this request
+	// is invalid in the current context, as Ledger was not expecting another
+	// script to be parsed
+	return ctx->remainingScripts[ctx->level] > 0;
+}
+
 bool shouldFinish()
 {
 	// we finish if there are no more scripts to be processed
@@ -31,6 +40,7 @@ bool shouldFinish()
 static inline void scriptFinished()
 {
 	--ctx->remainingScripts[ctx->level];
+	TRACE("native script finished, level = %u, remainingScripts = %u", ctx->level, ctx->remainingScripts[ctx->level]);
 }
 
 static inline void complexScriptFinished()
@@ -39,38 +49,102 @@ static inline void complexScriptFinished()
 	scriptFinished();
 }
 
+// UI
+
+static void deriveScriptHash_display_ui_callback()
+{
+	if (shouldFinish()) {
+		TRACE("whole native script received");
+		ui_idle();
+		// TODO: hashing, this is just a mock response
+		uint8_t buffer[28] = {0};
+		io_send_buf(SUCCESS, buffer, 28);
+	} else {
+		io_send_buf(SUCCESS, NULL, 0);
+	}
+}
+
 // Start complex native script
 
-void deriveNativeScriptHash_handleAllOrAny(read_view_t* view)
+void deriveNativeScriptHash_handleAll(read_view_t* view)
 {
 	// Parse data
 	ctx->remainingScripts[ctx->level] = parse_u4be(view);
+	ctx->type[ctx->level] = NATIVE_COMPLEX_SCRIPT_ALL;
 
 	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
 	TRACE("level = %u, remaining scripts = %u", ctx->level, ctx->remainingScripts[ctx->level]);
+
+	// max possible length 26: "Confirm x scripts"
+	// where x is 2^32-1
+	char text[27];
+	explicit_bzero(text, SIZEOF(text));
+	snprintf(text, SIZEOF(text), "Confirm %d scripts", ctx->remainingScripts[ctx->level]);
+	ASSERT(strlen(text) + 1 < SIZEOF(text));
+
+	ui_displayPaginatedText(
+		// TODO: proper UI screen headers with numbered sections
+		"Script - ALL",
+		text,
+		&deriveScriptHash_display_ui_callback
+	);
+}
+
+void deriveNativeScriptHash_handleAny(read_view_t* view)
+{
+	// Parse data
+	ctx->remainingScripts[ctx->level] = parse_u4be(view);
+	ctx->type[ctx->level] = NATIVE_COMPLEX_SCRIPT_ANY;
+
+	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
+	TRACE("level = %u, remaining scripts = %u", ctx->level, ctx->remainingScripts[ctx->level]);
+
+	// max possible length 26: "Confirm x scripts"
+	// where x is 2^32-1
+	char text[27];
+	explicit_bzero(text, SIZEOF(text));
+	snprintf(text, SIZEOF(text), "Confirm %d scripts", ctx->remainingScripts[ctx->level]);
+	ASSERT(strlen(text) + 1 < SIZEOF(text));
+
+	ui_displayPaginatedText(
+		"Script - ANY",
+		text,
+		&deriveScriptHash_display_ui_callback
+	);
 }
 
 void deriveNativeScriptHash_handleNofK(read_view_t* view)
 {
 	// parse data
-	uint32_t requiredScripts = parse_u4be(view);
+	ctx->nativeScript.nOfK.n = parse_u4be(view);
 	ctx->remainingScripts[ctx->level] = parse_u4be(view);
+	ctx->type[ctx->level] = NATIVE_COMPLEX_SCRIPT_N_OF_K;
 
 	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
-	TRACE("level = %u, required scripts = %u, remaining scripts = %u", ctx->level, requiredScripts, ctx->remainingScripts[ctx->level]);
+	TRACE("level = %u, required scripts = %u, remaining scripts = %u", ctx->level, ctx->nativeScript.nOfK.n, ctx->remainingScripts[ctx->level]);
 	
 	// validate that the received requiredScripts count makes sense
-	VALIDATE(ctx->remainingScripts[ctx->level] >= requiredScripts, ERR_INVALID_DATA);
+	VALIDATE(ctx->remainingScripts[ctx->level] >= ctx->nativeScript.nOfK.n, ERR_INVALID_DATA);
+
+	// max possible length 32: "Confirm x of x"
+	// where x is 2^32-1
+	char text[33];
+	explicit_bzero(text, SIZEOF(text));
+	snprintf(text, SIZEOF(text), "Confirm %d of %d", ctx->nativeScript.nOfK.n, ctx->remainingScripts[ctx->level]);
+	ASSERT(strlen(text) + 1 < SIZEOF(text));
+
+	ui_displayPaginatedText(
+		"Script - N of K",
+		text,
+		&deriveScriptHash_display_ui_callback
+	);
 }
 
 void deriveNativeScriptHash_handleComplexScriptStart(read_view_t* view)
 {
 	TRACE_BUFFER(view->begin, (size_t) (view->end - view->begin));
 
-	// if the number of remaining scripts is not bigger than 0, then this request
-	// is invalid in the current context, as Ledger was not expecting another
-	// script to be parsed
-	VALIDATE(ctx->remainingScripts[ctx->level] > 0, ERR_INVALID_STATE);
+	VALIDATE(expectsRequest(), ERR_INVALID_STATE);
 
 	// check if we can increase the level without breaking the MAX_SCRIPT_DEPTH constraint
 	VALIDATE(ctx->level + 1 < MAX_SCRIPT_DEPTH, ERR_DATA_TOO_LARGE);
@@ -81,8 +155,8 @@ void deriveNativeScriptHash_handleComplexScriptStart(read_view_t* view)
 
 	switch(nativeScriptType) {
 #	define  CASE(TYPE, HANDLER) case TYPE: HANDLER(view); break;
-		CASE(NATIVE_SCRIPT_ALL, deriveNativeScriptHash_handleAllOrAny);
-		CASE(NATIVE_SCRIPT_ANY, deriveNativeScriptHash_handleAllOrAny);
+		CASE(NATIVE_SCRIPT_ALL, deriveNativeScriptHash_handleAll);
+		CASE(NATIVE_SCRIPT_ANY, deriveNativeScriptHash_handleAny);
 		CASE(NATIVE_SCRIPT_N_OF_K, deriveNativeScriptHash_handleNofK);
 		default: THROW(ERR_INVALID_DATA);
 #	undef   CASE
@@ -93,28 +167,41 @@ void deriveNativeScriptHash_handleComplexScriptStart(read_view_t* view)
 
 void deriveNativeScriptHash_handleDeviceOwnedPubkey(read_view_t* view)
 {
-	bip44_path_t path;
-	view_skipBytes(view, bip44_parseFromWire(&path, VIEW_REMAINING_TO_TUPLE_BUF_SIZE(view)));
+	bip44_path_t *path = &ctx->nativeScript.pubkeyDeviceOwned.path;
+	view_skipBytes(view, bip44_parseFromWire(path, VIEW_REMAINING_TO_TUPLE_BUF_SIZE(view)));
 
 	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
 	TRACE("Pubkey given by path:");
-	BIP44_PRINTF(&path);
+	BIP44_PRINTF(path);
 	PRINTF("\n");
+
+	ui_displayPathScreen(
+		"Script - key path",
+		&ctx->nativeScript.pubkeyDeviceOwned.path,
+		&deriveScriptHash_display_ui_callback
+	);
 }
 
 void deriveNativeScriptHash_handleThirdPartyPubkey(read_view_t* view)
 {
-	uint8_t pubkeyHash[ADDRESS_KEY_HASH_LENGTH];
+	uint8_t *hash = ctx->nativeScript.pubkeyThirdParty.hash;
 	VALIDATE(view_remainingSize(view) == ADDRESS_KEY_HASH_LENGTH, ERR_INVALID_DATA);
-	view_memmove(pubkeyHash, view, ADDRESS_KEY_HASH_LENGTH);
+	view_memmove(hash, view, ADDRESS_KEY_HASH_LENGTH);
 
 #	ifdef DEVEL
 	TRACE("Pubkey given by hash:");
 	for (int i = 0; i < ADDRESS_KEY_HASH_LENGTH; i++) {
-		PRINTF("%x", pubkeyHash[i]);
+		PRINTF("%x", hash[i]);
 	}
 	PRINTF("\n");
 #	endif
+
+	ui_displayHexBufferScreen(
+		"Script - key",
+		ctx->nativeScript.pubkeyThirdParty.hash,
+		ADDRESS_KEY_HASH_LENGTH,
+		&deriveScriptHash_display_ui_callback
+	);
 }
 
 void deriveNativeScriptHash_handlePubkey(read_view_t* view)
@@ -132,33 +219,44 @@ void deriveNativeScriptHash_handlePubkey(read_view_t* view)
 	}
 }
 
+#define SPLIT_UINT64(n) (uint32_t) (n >> 32), (uint32_t) n
+
 void deriveNativeScriptHash_handleInvalidBefore(read_view_t* view)
 {
-	uint64_t timelock = parse_u8be(view);
+	ctx->nativeScript.invalidBefore.timelock = parse_u8be(view);
 
 	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
 	// PRINTF doesn't handle uint64_t so we split it into two uint32_t
-	TRACE("invalid_before timelock = 0x%x%x", (uint32_t) (timelock >> 32), (uint32_t) timelock);
-	UNUSED(timelock);
+	TRACE("invalid_before timelock = 0x%x%x", SPLIT_UINT64(ctx->nativeScript.invalidBefore.timelock));
+
+	ui_displayUint64Screen(
+		"Script - invalid before",
+		ctx->nativeScript.invalidBefore.timelock,
+		&deriveScriptHash_display_ui_callback
+	);
 }
 
 void deriveNativeScriptHash_handleInvalidHereafter(read_view_t* view)
 {
-	uint64_t timelock = parse_u8be(view);
+	ctx->nativeScript.invalidHereafter.timelock = parse_u8be(view);
 
 	VALIDATE(view_remainingSize(view) == 0, ERR_INVALID_DATA);
-	TRACE("invalid_hereafter timelock = 0x%x%x", (uint32_t) (timelock >> 32), (uint32_t) timelock);
-	UNUSED(timelock);
+	TRACE("invalid_hereafter timelock = 0x%x%x", SPLIT_UINT64(ctx->nativeScript.invalidHereafter.timelock));
+
+	ui_displayUint64Screen(
+		"Script - invalid hereafter",
+		ctx->nativeScript.invalidHereafter.timelock,
+		&deriveScriptHash_display_ui_callback
+	);
 }
+
+#undef SPLIT_UINT64
 
 void deriveNativeScriptHash_handleSimpleScript(read_view_t* view)
 {
 	TRACE_BUFFER(view->begin, (size_t) (view->end - view->begin));
 
-	// if the number of remaining scripts is not bigger than 0, then this request
-	// is invalid in the current context, as Ledger was not expecting another
-	// script to be parsed
-	VALIDATE(ctx->remainingScripts[ctx->level] > 0, ERR_INVALID_STATE);
+	VALIDATE(expectsRequest(), ERR_INVALID_STATE);
 
 	uint8_t nativeScriptType = parse_u1be(view);
 	TRACE("native simple script type = %u", nativeScriptType);
@@ -185,6 +283,15 @@ void deriveNativeScriptHash_handleComplexScriptFinish(read_view_t* view)
 	// if the number of remaining scripts for the current complex script is not 0
 	// then this request is invalid in the current context
 	VALIDATE(ctx->remainingScripts[ctx->level] == 0, ERR_INVALID_STATE);
+
+	switch(ctx->type[ctx->level]) {
+#	define  CASE(TYPE, HEADER) case TYPE: ui_displayPaginatedText(HEADER, "Finished", &deriveScriptHash_display_ui_callback); break;
+	CASE(NATIVE_COMPLEX_SCRIPT_ALL, "Script - ALL");
+	CASE(NATIVE_COMPLEX_SCRIPT_ANY, "Script - ANY");
+	CASE(NATIVE_COMPLEX_SCRIPT_N_OF_K, "Script - N of K");
+	default: THROW(ERR_INVALID_STATE);
+#	undef CASE
+	}
 
 	complexScriptFinished();
 }
@@ -223,16 +330,12 @@ void deriveNativeScriptHash_handleAPDU(
 		ctx->remainingScripts[ctx->level] = 1;
 	}
 
+	// reset the native script object
+	explicit_bzero(&ctx->nativeScript, SIZEOF(ctx->nativeScript));
+
 	read_view_t view = make_read_view(wireDataBuffer, wireDataBuffer + wireDataSize);
 
 	subhandler_fn_t* subhandler = lookup_subhandler(p1);
 	VALIDATE(subhandler != NULL, ERR_INVALID_REQUEST_PARAMETERS);
 	subhandler(&view);
-
-	if (shouldFinish()) {
-		TRACE("whole native script received");
-	}
-
-	// TODO: temporary solution for response
-	io_send_buf(SUCCESS, NULL, 0);
 }
--- src/deriveNativeScriptHash.h
+++ src/deriveNativeScriptHash.h
@@ -1,6 +1,8 @@
 #ifndef H_CARDANO_APP_DERIVE_NATIVE_SCRIPT_HASH
 #define H_CARDANO_APP_DERIVE_NATIVE_SCRIPT_HASH
 
+#include "bip44.h"
+#include "cardano.h"
 #include "common.h"
 #include "handlers.h"
 
@@ -10,8 +12,44 @@ handler_fn_t deriveNativeScriptHash_handleAPDU;
 static const size_t MAX_SCRIPT_DEPTH = 11;
 
 typedef struct {
+	bip44_path_t path;
+} native_script_pubkey_device_owned_t;
+
+typedef struct {
+	uint8_t hash[ADDRESS_KEY_HASH_LENGTH];
+} native_script_pubkey_third_party_t;
+
+typedef struct {
+	uint32_t n;
+} native_script_n_of_k_t;
+
+typedef struct {
+	uint64_t timelock;
+} native_script_invalid_before_t;
+
+typedef struct {
+	uint64_t timelock;
+} native_script_invalid_hereafter_t;
+
+typedef union {
+	native_script_pubkey_device_owned_t pubkeyDeviceOwned;
+	native_script_pubkey_third_party_t pubkeyThirdParty;
+	native_script_n_of_k_t nOfK;
+	native_script_invalid_before_t invalidBefore;
+	native_script_invalid_hereafter_t invalidHereafter;
+} native_script_t;
+
+typedef enum {
+	NATIVE_COMPLEX_SCRIPT_ALL,
+	NATIVE_COMPLEX_SCRIPT_ANY,
+	NATIVE_COMPLEX_SCRIPT_N_OF_K,
+} native_complex_script_type;
+
+typedef struct {
 	uint8_t level;
 	uint32_t remainingScripts[MAX_SCRIPT_DEPTH];
+	native_complex_script_type type[MAX_SCRIPT_DEPTH];
+	native_script_t nativeScript;
 } ins_derive_native_script_hash_context_t;
 
 #endif // H_CARDANO_APP_DERIVE_NATIVE_SCRIPT_HASH
