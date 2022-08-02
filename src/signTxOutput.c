e2dc89eb168e894ba25fde499e42dbf0c66f4c35
rustammendel <rustammendel@protonmail.com>
2022-08-02 17:03:37 +0200

    Add ui machine for script ref

--- src/signTxOutput.c
+++ src/signTxOutput.c
@@ -474,8 +474,8 @@ static void parseTopLevelData(const uint8_t* wireDataBuffer, size_t wireDataSize
 		TRACE("includeDatum = %d", (int) subctx->includeDatum);
 
 		// TODO should be allowed
-		 subctx->includeScriptRef = signTx_parseIncluded(parse_u1be(&view));
-		 TRACE("includeScriptRef = %d", (int) subctx->includeScriptRef);
+		subctx->includeScriptRef = signTx_parseIncluded(parse_u1be(&view));
+		TRACE("includeScriptRef = %d", (int) subctx->includeScriptRef);
 
 		if (subctx->includeDatum || subctx->includeScriptRef) {
 			// it's easier to verify all Plutus-related things via txid all at once
@@ -1182,9 +1182,49 @@ static void handleDatumChunkAPDU(const uint8_t* wireDataBuffer, size_t wireDataS
 	}
 }
 
-// ========================== REFERENCE SCRIPT =============================
+// ========================== SCRIPT REFERENCE =============================
 
-static void handleRefScriptAPDU(const uint8_t* wireDataBuffer, size_t wireDataSize)
+enum {
+	HANDLE_SCRIPT_REF_STEP_DISPLAY = 3600,
+	HANDLE_SCRIPT_REF_STEP_RESPOND,
+	HANDLE_SCRIPT_REF_STEP_INVALID,
+};
+
+static void handleRefScript_ui_runStep()
+{
+	output_context_t* subctx = accessSubcontext();
+	TRACE("UI step %d", subctx->ui_step);
+	ui_callback_fn_t* this_fn = handleRefScript_ui_runStep;
+
+	UI_STEP_BEGIN(subctx->ui_step, this_fn);
+	UI_STEP(HANDLE_SCRIPT_REF_STEP_DISPLAY) {
+		char l1[30];
+		size_t scriptSize = subctx->stateData.scriptRefRemainingBytes + subctx->stateData.scriptRefChunkSize;
+		// scriptSize with 6 digits fits on the screen, less than max tx size
+		// if more is needed, "bytes" can be replaced by "B" for those larger numbers
+		snprintf(l1, SIZEOF(l1), "Script %u bytes", scriptSize);
+		ASSERT(strlen(l1) + 1 < SIZEOF(l1));
+
+		char l2[20];
+		size_t prefixLength = MIN(subctx->stateData.scriptRefChunkSize, 6);
+		size_t len = encode_hex(subctx->stateData.scriptChunk, prefixLength, l2, SIZEOF(l2));
+		snprintf(l2 + len, SIZEOF(l2) - len, "...");
+		ASSERT(strlen(l2) + 1 < SIZEOF(l2));
+
+		ui_displayPaginatedText(
+		        l1,
+		        l2,
+		        this_fn
+		);
+	}
+	UI_STEP(HANDLE_SCRIPT_REF_STEP_RESPOND) {
+		respondSuccessEmptyMsg();
+		advanceState();
+	}
+	UI_STEP_END(HANDLE_SCRIPT_REF_STEP_INVALID);
+}
+
+static void handleScriptRefAPDU(const uint8_t* wireDataBuffer, size_t wireDataSize)
 {
 	{
 		// sanity checks
@@ -1216,7 +1256,7 @@ static void handleRefScriptAPDU(const uint8_t* wireDataBuffer, size_t wireDataSi
 	}
 	{
 		// add to tx
-		TRACE("Adding script reference  to tx hash");
+		TRACE("Adding script reference to tx hash");
 		txHashBuilder_addOutput_scriptReference(
 		        &BODY_CTX->txHashBuilder,
 		        subctx->stateData.scriptRefRemainingBytes
@@ -1231,26 +1271,21 @@ static void handleRefScriptAPDU(const uint8_t* wireDataBuffer, size_t wireDataSi
 		subctx->stateData.scriptRefRemainingBytes -= subctx->stateData.scriptRefChunkSize;
 	}
 	{
-		// TODO all of this
-
 		// select UI step
-		security_policy_t policy = policyForSignTxOutputDatumHash(subctx->outputSecurityPolicy);
+		security_policy_t policy = policyForSignTxOutputScriptReference(subctx->outputSecurityPolicy);
 		TRACE("Policy: %d", (int) policy);
 		ENSURE_NOT_DENIED(policy);
 
 		switch (policy) {
 #	define  CASE(POLICY, UI_STEP) case POLICY: {subctx->ui_step=UI_STEP; break;}
-			CASE(POLICY_SHOW_BEFORE_RESPONSE, HANDLE_DATUM_HASH_STEP_DISPLAY);
-			CASE(POLICY_ALLOW_WITHOUT_PROMPT, HANDLE_DATUM_HASH_STEP_RESPOND);
+			CASE(POLICY_SHOW_BEFORE_RESPONSE, HANDLE_SCRIPT_REF_STEP_DISPLAY);
+			CASE(POLICY_ALLOW_WITHOUT_PROMPT, HANDLE_SCRIPT_REF_STEP_RESPOND);
 #	undef   CASE
 		default:
 			THROW(ERR_NOT_IMPLEMENTED);
 		}
 
-		// TODO
-		respondSuccessEmptyMsg();
-		advanceState();
-		//signTxOutput_handleDatumInline_ui_runStep();
+		handleRefScript_ui_runStep();
 	}
 }
 
@@ -1284,8 +1319,8 @@ static void handleScriptRefChunkAPDU(const uint8_t* wireDataBuffer, size_t wireD
 		// add to tx
 		TRACE("Adding inline datum chunk to tx hash");
 		txHashBuilder_addOutput_scriptReference_dataChunk(
-				&BODY_CTX->txHashBuilder,
-				subctx->stateData.scriptChunk, subctx->stateData.scriptRefChunkSize
+		        &BODY_CTX->txHashBuilder,
+		        subctx->stateData.scriptChunk, subctx->stateData.scriptRefChunkSize
 		);
 	}
 	respondSuccessEmptyMsg();
