359b16ddcf83e532e759980a2ba7adf7ebdafefd
Xavier Chapron <xavier.chapron@ledger.fr>
2023-06-28 11:41:06 +0200

    update crypto calls

--- src/keyDerivation_test.c
+++ src/keyDerivation_test.c
@@ -10,67 +10,6 @@ static void pathSpec_init(bip44_path_t* pathSpec, const uint32_t* pathArray, uin
 	memmove(pathSpec->path, pathArray, pathLength * 4);
 }
 
-void testcase_derivePrivateKey(uint32_t* path, uint32_t pathLen, const char* expectedHex)
-{
-	PRINTF("testcase_derivePrivateKey ");
-
-	bip44_path_t pathSpec;
-	ASSERT(pathLen <= BIP44_MAX_PATH_ELEMENTS);
-	pathSpec_init(&pathSpec, path, pathLen);
-	BIP44_PRINTF(&pathSpec);
-	PRINTF("\n");
-
-	uint8_t expected[64] = {0};
-	size_t expectedSize = decode_hex(expectedHex, expected, SIZEOF(expected));
-
-	chain_code_t chainCode;
-	privateKey_t privateKey;
-	derivePrivateKey(&pathSpec, &chainCode, &privateKey);
-	EXPECT_EQ_BYTES(expected, privateKey.d, expectedSize);
-}
-
-void testPrivateKeyDerivation()
-{
-
-#define HD HARDENED_BIP32
-
-#define TESTCASE(path_, expectedHex_) \
-	{ \
-		uint32_t path[] = { UNWRAP path_ }; \
-		testcase_derivePrivateKey(path, ARRAY_LEN(path), expectedHex_); \
-	}
-
-
-	// Note: Failing tests here? Did you load testing mnemonic
-	// "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"?
-
-	TESTCASE(
-	        (HD + 44, HD + 1815, HD + 1),
-
-	        "5878bdf06af259f1419324680c4d4ce05b4343895b697144d874749925e69d41"
-	        "648e7e50061809207f81dac36b73cd8fab2325d49d26bc65ebbb696ea59c45b8"
-	);
-
-	TESTCASE(
-	        (HD + 1852, HD + 1815, HD + 1, 0, HD + 55),
-
-	        "08671b6f39031f2fc7bcebff7b85aec4c27787fc6a12f7f8bbfb28902ae69d41"
-	        "85751aae1484fb26c53f8e4c35b943d11449b52e5863779821fcc49cddf60792"
-	);
-#undef TESTCASE
-
-#define TESTCASE(path_, error_) \
-	{ \
-		uint32_t path[] = { UNWRAP path_ }; \
-		EXPECT_THROWS(testcase_derivePrivateKey(path, ARRAY_LEN(path), ""), error_ ); \
-	}
-
-	TESTCASE( (HD + 43, HD + 1815), ERR_ASSERT);
-	TESTCASE( (HD + 44, 1815, HD + 1), ERR_ASSERT);
-	TESTCASE( (HD + 44, HD + 33, HD + 1), ERR_ASSERT);
-#undef TESTCASE
-}
-
 void testcase_derivePublicKey(uint32_t* path, uint32_t pathLen, const char* expected)
 {
 	PRINTF("testcase_derivePublicKey ");
@@ -81,18 +20,15 @@ void testcase_derivePublicKey(uint32_t* path, uint32_t pathLen, const char* expe
 	BIP44_PRINTF(&pathSpec);
 	PRINTF("\n");
 
-	chain_code_t chainCode;
-	privateKey_t privateKey;
-	derivePrivateKey(&pathSpec, &chainCode, &privateKey);
-	cx_ecfp_public_key_t publicKey;
-	deriveRawPublicKey(&privateKey, &publicKey);
-	uint8_t publicKeyRaw[32] = {0};
-	extractRawPublicKey(&publicKey, publicKeyRaw, SIZEOF(publicKeyRaw));
+	extendedPublicKey_t extPubKey;
+	deriveExtendedPublicKey(&pathSpec, &extPubKey);
 
 	uint8_t expectedBuffer[32] = {0};
 	decode_hex(expected, expectedBuffer, SIZEOF(expectedBuffer));
-	EXPECT_EQ_BYTES(expectedBuffer, publicKeyRaw, SIZEOF(expectedBuffer));
+	EXPECT_EQ_BYTES(expectedBuffer, extPubKey.pubKey, SIZEOF(expectedBuffer));
 }
+
+#define HD HARDENED_BIP32
 
 void testPublicKeyDerivation()
 {
@@ -137,57 +73,12 @@ void testPublicKeyDerivation()
 }
 
 
-void testcase_deriveChainCode(uint32_t* path, uint32_t pathLen, const char* expectedHex)
-{
-	PRINTF("testcase_deriveChainCode ");
-
-	chain_code_t chainCode;
-	privateKey_t privateKey;
-
-	bip44_path_t pathSpec;
-	pathSpec_init(&pathSpec, path, pathLen);
-
-	BIP44_PRINTF(&pathSpec);
-	PRINTF("\n");
-
-	derivePrivateKey(&pathSpec, &chainCode, &privateKey);
-
-	uint8_t expectedBuffer[32] = {0};
-	decode_hex(expectedHex, expectedBuffer, 32);
-	EXPECT_EQ_BYTES(expectedBuffer, chainCode.code, 32);
-}
-
-// not tested
-void testChainCodeDerivation()
-{
-#define TESTCASE(path_, expectedHex_) \
-	{ \
-		uint32_t path[] = { UNWRAP path_ }; \
-		testcase_deriveChainCode(path, ARRAY_LEN(path), expectedHex_); \
-	}
-
-	TESTCASE(
-	        (HD + 44, HD + 1815, HD + 1),
-	        "0b161cb11babe1f56c3f9f1cbbb7b6d2d13eeb3efa67205198a69b8d81885354"
-	);
-
-	TESTCASE(
-	        (HD + 1852, HD + 1815, HD + 1, HD + 1, HD + 44),
-	        "a92fb06ab6a4321d4b55878ec062988f315d9fe701009946cd95617dde8ba2a1"
-	);
-
-#undef TESTCASE
-}
-
-
 void run_key_derivation_test()
 {
 	PRINTF("Running key derivation tests\n");
 	PRINTF("If they fail, make sure you seeded your device with\n");
 	PRINTF("12-word mnemonic: 11*abandon about\n");
-	testPrivateKeyDerivation();
 	testPublicKeyDerivation();
-	testChainCodeDerivation();
 }
 
 #endif // DEVEL
--- src/messageSigning.c
+++ src/messageSigning.c
@@ -2,66 +2,42 @@
 
 #include "messageSigning.h"
 #include "cardano.h"
-#include "keyDerivation.h"
 #include "bip44.h"
-
-static void signRawMessage(privateKey_t* privateKey,
-                           const uint8_t* messageBuffer, size_t messageSize,
-                           uint8_t* outBuffer, size_t outSize)
-{
-	uint8_t signature[64] = {0};
-	ASSERT(messageSize < BUFFER_SIZE_PARANOIA);
-	ASSERT(outSize == SIZEOF(signature));
-
-	#ifndef FUZZING
-	// Note(ppershing): this could be done without
-	// temporary copy
-	STATIC_ASSERT(sizeof(int) == sizeof(size_t), "bad sizing");
-	io_seproxyhal_io_heartbeat();
-	size_t signatureSize =
-	        (size_t) cx_eddsa_sign(
-	                (const struct cx_ecfp_256_private_key_s*) privateKey,
-	                0 /* mode */,
-	                CX_SHA512,
-	                messageBuffer, messageSize,
-	                NULL /* ctx */, 0 /* ctx len */,
-	                signature, SIZEOF(signature),
-	                0 /* info */
-	        );
-	io_seproxyhal_io_heartbeat();
-
-	ASSERT(signatureSize == ED25519_SIGNATURE_LENGTH);
-	memmove(outBuffer, signature, signatureSize);
-	#endif
-}
+#include "securityPolicy.h"
+#include "crypto.h"
 
 static void signRawMessageWithPath(bip44_path_t* pathSpec,
                                    const uint8_t* messageBuffer, size_t messageSize,
                                    uint8_t* outBuffer, size_t outSize)
 {
+	size_t sigLen = outSize;
+
 	ASSERT(messageSize < BUFFER_SIZE_PARANOIA);
-	ASSERT(outSize < BUFFER_SIZE_PARANOIA);
+	ASSERT(sigLen == ED25519_SIGNATURE_LENGTH);
 
-	chain_code_t chainCode;
-	privateKey_t privateKey;
+	// Sanity check
+	ASSERT(pathSpec->length <= ARRAY_LEN(pathSpec->path));
 
-	TRACE("derive private key");
+	// if the path is invalid, it's a bug in previous validation
+	ASSERT(policyForDerivePrivateKey(pathSpec) != POLICY_DENY);
 
-	BEGIN_TRY {
-		TRY {
-			derivePrivateKey(pathSpec, &chainCode, &privateKey);
-
-			signRawMessage(
-			        &privateKey,
-			        messageBuffer, messageSize,
-			        outBuffer, outSize
-			);
+	#ifndef FUZZING
+	{
+		cx_err_t error = crypto_eddsa_sign(pathSpec->path,
+		                                   pathSpec->length,
+		                                   messageBuffer,
+		                                   messageSize,
+		                                   outBuffer,
+		                                   &sigLen);
+		if (error != CX_OK) {
+			PRINTF("error: %d", error);
+			ASSERT(false);
 		}
-		FINALLY {
-			explicit_bzero(&privateKey, SIZEOF(privateKey));
-			explicit_bzero(&chainCode, SIZEOF(chainCode));
-		}
-	} END_TRY;
+	}
+	#endif
+
+	ASSERT(sigLen == ED25519_SIGNATURE_LENGTH);
+
 }
 
 // sign the given hash by the private key derived according to the given path
