LedgerHQ/app-cardano

Implement 1st ragger tests (Version, Serial, App Name)

tests/application_client/app_def.py
@@ -1,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Ledger SAS
+# SPDX-License-Identifier: LicenseRef-LEDGER
+"""
+This module provides Ragger tests Client application.
+It contains the application definitions.
+"""
+from enum import IntEnum
+
+
+class Errors(IntEnum):
+    """Application Errors definitions"""
+
+    SW_AUTORESPOND_START          = 0x6E00
+    SW_MALFORMED_REQUEST_HEADER   = 0x6E01
+    SW_BAD_CLA                    = 0x6E02
+    SW_UNKNOWN_INS                = 0x6E03
+    SW_STILL_IN_CALL              = 0x6E04
+    SW_INVALID_REQUEST_PARAMETERS = 0x6E05
+    SW_INVALID_STATE              = 0x6E06
+    SW_INVALID_DATA               = 0x6E07
+    SW_REJECTED_BY_USER           = 0x6E09
+    SW_REJECTED_BY_POLICY         = 0x6E10
+    SW_DEVICE_LOCKED              = 0x6E11
+    SW_AUTORESPOND_END            = 0x6E13
+    SW_SUCCESS                    = 0x9000
+
+
+class InsType(IntEnum):
+    GET_VERSION = 0x00
+    GET_SERIAL = 0x01
+    GET_PUBLIC_ADDR = 0x10
+    DERIVE_PUBLIC_ADDR = 0x11
+    DERIVE_SCRIPT_HASH = 0x12
+    SIGN_TX = 0x21
+    SIGN_OP_CERT = 0x22
+    SIGN_CIP36_VOTE = 0x23
+    SIGN_MSG = 0x24
+

tests/test_version.py
@@ -1,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Ledger SAS
+# SPDX-License-Identifier: LicenseRef-LEDGER
+"""
+This module provides Ragger tests for Version/Serial check
+"""
+
+from ragger.utils.misc import get_current_app_name_and_version
+from ragger.backend import BackendInterface
+
+from application_client.command_sender import CommandSender
+
+from utils import verify_name, verify_version
+
+
+def test_check_version(backend: BackendInterface) -> None:
+    """Check version and name, returned by the OS"""
+
+    # Send the APDU
+    app_name, version = get_current_app_name_and_version(backend)
+    print(f" Name: {app_name}")
+    print(f" Version: {version}")
+    verify_name(app_name)
+    verify_version(version)
+
+
+def test_check_app_version(backend: BackendInterface) -> None:
+    """Check version and name, returned by the App"""
+
+    # Use the app interface instead of raw interface
+    client = CommandSender(backend)
+    # Send the APDU
+    version = client.get_version()
+
+    print(f" Version: {version.hex()}")
+    vers_str = f"{version[0]}.{version[1]}.{version[2]}"
+    verify_version(vers_str)
+
+
+def test_check_app_serial(backend: BackendInterface) -> None:
+    """Check App Serial"""
+
+    # Use the app interface instead of raw interface
+    client = CommandSender(backend)
+    # Send the APDU
+    serial = client.get_serial()
+
+    print(f" Version: {serial.hex()} -> {serial.decode()}")

vacuumlabs/ledger-app-cardano-shelley

tests/application_client/app_def.py
@@ -1,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Ledger SAS
+# SPDX-License-Identifier: LicenseRef-LEDGER
+"""
+This module provides Ragger tests Client application.
+It contains the application definitions.
+"""
+from enum import IntEnum
+
+
+class Errors(IntEnum):
+    """Application Errors definitions"""
+
+    SW_MALFORMED_REQUEST_HEADER   = 0x6E01
+    SW_BAD_CLA                    = 0x6E02
+    SW_UNKNOWN_INS                = 0x6E03
+    SW_STILL_IN_CALL              = 0x6E04
+    SW_INVALID_REQUEST_PARAMETERS = 0x6E05
+    SW_INVALID_STATE              = 0x6E06
+    SW_INVALID_DATA               = 0x6E07
+    SW_REJECTED_BY_USER           = 0x6E09
+    SW_REJECTED_BY_POLICY         = 0x6E10
+    SW_DEVICE_LOCKED              = 0x6E11
+    SW_SWAP_CHECKING_FAIL         = 0x6E13
+    SW_SUCCESS                    = 0x9000
+
+
+class InsType(IntEnum):
+    GET_VERSION = 0x00
+    GET_SERIAL = 0x01
+    GET_PUBLIC_ADDR = 0x10
+    DERIVE_PUBLIC_ADDR = 0x11
+    DERIVE_SCRIPT_HASH = 0x12
+    SIGN_TX = 0x21
+    SIGN_OP_CERT = 0x22
+    SIGN_CIP36_VOTE = 0x23
+    SIGN_MSG = 0x24
+

tests/test_version.py
@@ -1,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Ledger SAS
+# SPDX-License-Identifier: LicenseRef-LEDGER
+"""
+This module provides Ragger tests for Version/Serial check
+"""
+
+from ragger.utils.misc import get_current_app_name_and_version
+from ragger.backend import BackendInterface
+
+from application_client.command_sender import CommandSender
+
+from utils import verify_name, verify_version
+
+
+def test_check_version(backend: BackendInterface) -> None:
+    """Check version and name, returned by the OS"""
+
+    # Send the APDU
+    app_name, version = get_current_app_name_and_version(backend)
+    print(f" Name: {app_name}")
+    print(f" Version: {version}")
+    verify_name(app_name)
+    verify_version(version)
+
+
+def test_check_app_version(backend: BackendInterface, appFlags: dict) -> None:
+    """Check version and name, returned by the App"""
+
+    # Use the app interface instead of raw interface
+    client = CommandSender(backend)
+    # Send the APDU
+    version = client.get_version()
+
+    print(f" Version: {version.hex()}")
+    print(f" Flags:\n  Debug={appFlags['isDebug']}\n  AppXS={appFlags['isAppXS']}")
+    vers_str = f"{version[0]}.{version[1]}.{version[2]}"
+    verify_version(vers_str)
+
+
+def test_check_app_serial(backend: BackendInterface) -> None:
+    """Check App Serial"""
+
+    # Use the app interface instead of raw interface
+    client = CommandSender(backend)
+    # Send the APDU
+    serial = client.get_serial()
+
+    print(f" Serial: {serial.hex()} -> {serial.decode()}")